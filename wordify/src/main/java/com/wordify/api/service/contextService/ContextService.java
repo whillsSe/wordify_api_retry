34347916da7a657cf70b14f10aff37693ec2daef
yokoyomi <yokomizohiroshi@gmail.com>
2023-07-11 11:20:52 +0900

    0711firstCommit

--- wordify/src/main/java/com/wordify/api/service/contextService/ContextService.java
+++ wordify/src/main/java/com/wordify/api/service/contextService/ContextService.java
@@ -1,8 +1,10 @@
 package com.wordify.api.service.contextService;
 
+import java.sql.SQLException;
+
 import com.wordify.api.dto.ContextDto;
 import com.wordify.api.dto.params.ContextQuery;
 
 public interface ContextService {
-  public ContextDto getContext(ContextQuery query);
+  public ContextDto getContext(ContextQuery query) throws SQLException;
 }
