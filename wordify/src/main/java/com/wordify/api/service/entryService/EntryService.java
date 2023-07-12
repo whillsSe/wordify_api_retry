ea67aa372b38526cbc6e5b2b1816958b4bd78834
yokoyomi <yokomizohiroshi@gmail.com>
2023-07-12 17:57:07 +0900

    0712

--- wordify/src/main/java/com/wordify/api/service/entryService/EntryService.java
+++ wordify/src/main/java/com/wordify/api/service/entryService/EntryService.java
@@ -3,10 +3,13 @@ package com.wordify.api.service.entryService;
 import java.sql.SQLException;
 import java.util.List;
 
+import com.wordify.api.dto.DefinitionDto;
 import com.wordify.api.dto.EntryDto;
 import com.wordify.api.dto.params.EntryQuery;
 
 public interface EntryService {
     //取得メソッドList<EntryDto>
     public List<EntryDto> getEntries(EntryQuery query) throws SQLException;
+
+    public DefinitionDto registerDefinition(DefinitionDto requestedValue);
 }
